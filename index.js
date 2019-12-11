d8ae53ad1210fab3b98621e9dd5afaac8b772855
YanEr <lx@qcmail.cn>
2019-12-11 18:25:54 +0800

    去掉http，统一使用request

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const through = require('through');
-const http = require('http');
 const request = require('request')
 const gulpUtil = require('gulp-util');
 const PluginError = gulpUtil.PluginError;
@@ -24,10 +23,7 @@ function gulpQcIconfont(options) {
 
   var downloadFont = function (index) {
     const EXT = FONT_EXT_LIST[index]
-    request({
-      url: 'http:' + URL_PREFIX + EXT,
-      encoding: null
-    }, (err, res, body) => {
+    request('http:' + URL_PREFIX + EXT, (err, res, body) => {
       const ICON_FILE = new gulpUtil.File({
         path: FONT_PATH + EXT,
         contents: Buffer.from(body)
@@ -45,35 +41,35 @@ function gulpQcIconfont(options) {
     }
   }
 
-  http.get('http:' + URL, function (response) {
-    response.setEncoding('binary'); // 二进制binary
-    var rawData = '';
-    response.on('data', function (chunk) {
-      rawData += chunk;
-    })
-    response.on('end', function () {
-      if (!IS_DEV) rawData = rawData.replace(new RegExp(URL_PREFIX, 'g'), FONT_PATH)
-      var result = '/* 字体图标，来源路径："' + URL + '"*/ \r\n';
-      var delUnnecessary = rawData.replace(/\.iconfont[\s\S]*?\}/, '');
-      var iconCss = delUnnecessary.match(/\.icon\-[\s\S]*?\}/g);
-      var handlerData = KEEP_ICON_FONT_STYLE ? rawData : delUnnecessary
-      result += handlerData.replace(/\.icon\-[\s\S]*?\}/g, '');
-      for (var i in iconCss) {
-        var item = iconCss[i];
-        if (ICON_PREFIX) item = item.replace(/\.icon\-/, ICON_PREFIX);
-        result += item + '\r\n';
-      }
-      result = result.replace(/\r{2,}/g, '\r');
-      result = result.replace(/\n{2,}/g, '\n');
+  request({
+    url: 'http:' + URL,
+    encoding: 'binary'
+  }, (err, res, body) => {
+    let rawData = body
+    if (!IS_DEV) rawData = rawData.replace(new RegExp(URL_PREFIX, 'g'), FONT_PATH)
+    var result = '/* 字体图标，来源路径："' + URL + '"*/ \r\n';
+    var delUnnecessary = rawData.replace(/\.iconfont[\s\S]*?\}/, '');
+    var iconCss = delUnnecessary.match(/\.icon\-[\s\S]*?\}/g);
+    var handlerData = KEEP_ICON_FONT_STYLE ? rawData : delUnnecessary
+    result += handlerData.replace(/\.icon\-[\s\S]*?\}/g, '');
+    for (var i in iconCss) {
+      var item = iconCss[i];
+      if (ICON_PREFIX) item = item.replace(/\.icon\-/, ICON_PREFIX);
+      result += item + '\r\n';
+    }
+    result = result.replace(/\r{2,}/g, '\r');
+    result = result.replace(/\n{2,}/g, '\n');
 
-      const CSS_FILE = new gulpUtil.File({
-        path: 'iconfont.css',
-        contents: Buffer.from(result)
-      });
-      stream.queue(CSS_FILE);
+    const CSS_FILE = new gulpUtil.File({
+      path: 'iconfont.css',
+      contents: Buffer.from(result)
+    });
+    stream.queue(CSS_FILE);
 
-      (!IS_DEV && FONT_EXT_LIST && FONT_EXT_LIST.length > 0) ? downloadFont(0): stream.emit('end')
-    })
+    (!IS_DEV && FONT_EXT_LIST && FONT_EXT_LIST.length > 0) ? downloadFont(0): stream.emit('end')
+  }, {
+    throttle: 1000,
+    delay: 1000
   })
   return stream;
 }
